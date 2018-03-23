61799c5096246b4d6a5fa4194e91e2b41352d9fb
xLife <xLifeFelix@users.noreply.github.com>
2018-03-23 18:17:10 +0100

    randomAI

---
+++ Connect4/src/v2/randomAi.java
@@ -0,0 +1,21 @@
+package v2;
+
+import java.util.Random;
+
+public class randomAi implements AI {
+	
+	private static Random rand = new Random();
+	
+	@Override
+	public int getInt(char[][] board, char player) {
+		
+		int num;
+		
+		do {
+			num = rand.nextInt(board.length) + 1;
+		} while (board[num - 1][0] != '.');
+		
+		return num;
+	}
+
+}
