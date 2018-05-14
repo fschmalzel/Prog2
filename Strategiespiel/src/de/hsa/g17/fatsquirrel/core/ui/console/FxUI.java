773f866f9310fe7fff6f04bebafe05b7b23589b7
gzazartur <richau33@gmail.com>
2018-05-14 14:36:52 +0200

    movecommand

--- Strategiespiel/src/de/hsa/g17/fatsquirrel/core/ui/console/FxUI.java
+++ Strategiespiel/src/de/hsa/g17/fatsquirrel/core/ui/console/FxUI.java
@@ -2,6 +2,7 @@ package de.hsa.g17.fatsquirrel.core.ui.console;
 
 import de.hsa.g17.fatsquirrel.core.BoardView;
 import de.hsa.g17.fatsquirrel.core.GameCommand;
+import de.hsa.g17.fatsquirrel.core.MoveCommand;
 import de.hsa.g17.fatsquirrel.core.UI;
 import de.hsa.g17.fatsquirrel.core.XY;
 import javafx.application.Platform;
@@ -17,75 +18,124 @@ import javafx.scene.paint.Color;
 
 public class FxUI extends Scene implements UI {
 
-    private static final int CELL_SIZE = 0;
+	private static final int CELL_SIZE = 50;
 	private Canvas boardCanvas;
 	private Label msgLabel;
-
+	private GameCommand gameCommand;
 
 	public FxUI(Parent parent, Canvas boardCanvas, Label msgLabel) {
-        super(parent);
-        this.boardCanvas = boardCanvas;
-        this.msgLabel = msgLabel;
-    }
-    
-    public static FxUI createInstance(XY boardSize) {
-        Canvas boardCanvas = new Canvas(boardSize.x() * CELL_SIZE, boardSize.y() * CELL_SIZE);
-        Label statusLabel = new Label();
-        VBox top = new VBox();
-        top.getChildren().add(boardCanvas);
-        top.getChildren().add(statusLabel);
-        statusLabel.setText("Hallo Welt");
-        final FxUI fxUI = new FxUI(top, boardCanvas, statusLabel); 
-        fxUI.setOnKeyPressed(
-                new EventHandler<KeyEvent>() {
-                   @Override
-                   public void handle(KeyEvent keyEvent) {
-                      System.out.println("Es wurde folgende Taste gedrückt: " + keyEvent.getCode() + " bitte behandeln!");
-                      // TODO handle event 
-                   }
-                }
-          );
-        return fxUI;
-    }
+		super(parent);
+		this.boardCanvas = boardCanvas;
+		this.msgLabel = msgLabel;
+	}
 
-    
+	public static FxUI createInstance(XY boardSize) {
+		Canvas boardCanvas = new Canvas(boardSize.x() * CELL_SIZE, boardSize.y() * CELL_SIZE);
+		Label statusLabel = new Label();
+		VBox top = new VBox();
+		top.getChildren().add(boardCanvas);
+		top.getChildren().add(statusLabel);
+		statusLabel.setText("Hallo Welt");
+		final FxUI fxUI = new FxUI(top, boardCanvas, statusLabel);
+		fxUI.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent keyEvent) {
+				switch (keyEvent.getCode()) {
+				case UP:
+					fxUI.setCommand(new MoveCommand(new XY(0,-1)));
+					break;
+				case DOWN:
+					fxUI.setCommand(new MoveCommand(new XY(0,1)));
+					break;
+				case LEFT:
+					fxUI.setCommand(new MoveCommand(new XY(-1,0)));
+					break;
+				case RIGHT:
+					fxUI.setCommand(new MoveCommand(new XY(1,0)));
+					break;
+				default:
+					break;
+				}
+			}
+		});
+		return fxUI;
+	}
 
-    @Override
-    public void render(final BoardView view) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                repaintBoardCanvas(view);            
-            }      
-        });  
-    }
-    
-    private void repaintBoardCanvas(BoardView view) {
-        GraphicsContext gc = boardCanvas.getGraphicsContext2D();
-        gc.clearRect(0, 0, boardCanvas.getWidth(), boardCanvas.getHeight());
-        XY viewSize = view.getSize();
+	@Override
+	public void render(final BoardView view) {
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				repaintBoardCanvas(view);
+			}
+		});
+	}
 
-        // dummy for rendering a board snapshot, TODO: change it!
-        gc.fillText("Where are the beasts?", 100, 100);
-        gc.setFill(Color.RED);
-        gc.fillOval(150, 150, 50, 50);
-    }
-    
+	private void repaintBoardCanvas(BoardView view) {
+		GraphicsContext gc = boardCanvas.getGraphicsContext2D();
+		gc.clearRect(0, 0, boardCanvas.getWidth(), boardCanvas.getHeight());
+		XY viewSize = view.getSize();
 
-    @Override
-    public void message(final String msg) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                msgLabel.setText(msg);            
-            }      
-        });         
-    }
+		for (int y = 0; y < viewSize.y(); y++)
+			for (int x = 0; x < viewSize.x(); x++) {
+				switch (view.getEntityType(new XY(x, y))) {
+				case BAD_BEAST:
+					gc.setFill(Color.RED);
+					gc.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+					break;
+				case BAD_PLANT:
+					gc.setFill(Color.ORANGE);
+					gc.fillOval(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+					break;
+				case EMPTY:
+					break;
+				case GOOD_BEAST:
+					gc.setFill(Color.GREEN);
+					gc.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+					break;
+				case GOOD_PLANT:
+					gc.setFill(Color.GREENYELLOW);
+					gc.fillOval(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+					break;
+				case MASTER_SQUIRREL:
+					gc.setFill(Color.BLUE);
+					gc.fillRoundRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, CELL_SIZE / 2, CELL_SIZE / 2);
+					break;
+				case MINI_SQUIRREL:
+					gc.setFill(Color.AQUAMARINE);
+					gc.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+					break;
+				case UNDEFINED:
+					break;
+				case WALL:
+					gc.setFill(Color.GREY);
+					gc.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+					break;
+				default:
+					break;
+				}
+			}
+
+	}
+
+	@Override
+	public void message(final String msg) {
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				msgLabel.setText(msg);
+			}
+		});
+	}
 
 	@Override
 	public GameCommand getCommand() {
-		// TODO Auto-generated method stub
-		return null;
+		GameCommand tmp = gameCommand;
+		gameCommand = null;
+		return tmp;
+	}
+	
+	private void setCommand(GameCommand gameCommand) {
+		this.gameCommand = gameCommand;
 	}
 }
-
