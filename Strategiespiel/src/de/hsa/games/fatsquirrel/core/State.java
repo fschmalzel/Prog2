60d8f690e7264e67680972e492f7b25bf9f04c74
xLife <xLifeFelix@users.noreply.github.com>
2018-06-10 18:16:26 +0200

    .

--- Strategiespiel/src/de/hsa/games/fatsquirrel/core/State.java
+++ Strategiespiel/src/de/hsa/games/fatsquirrel/core/State.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.logging.Logger;
@@ -18,7 +19,7 @@ public class State {
 	//TODO new highscore
 	private Board board;
 	private int steps;
-	private Map<String, Integer> scores;
+	private Map<String, List<Integer>> scores;
 	
 	MasterSquirrel masterSquirrel;
 	
@@ -27,39 +28,67 @@ public class State {
 
 		steps = boardConfig.getSteps();
 		board = new Board(boardConfig);
-	}
-	
-	private void endRound() {
+		
 		if (board.getPlayer() != null)
-			scores.put("player", board.getPlayer().getEnergy());
+			scores.put("player", new LinkedList<Integer>());
 		
 		for (MasterSquirrelBot bot : board.getMasterSquirrelBots()) {
 			String name = bot.getClass().getName();
 			if (name.startsWith("de.hsa.games.fatsquirrel.botimpls.")) {
 				name = name.substring("de.hsa.games.fatsquirrel.botimpls.".length());
 			}
-			scores.put(name, bot.getEnergy());
+			scores.put(name,  new LinkedList<Integer>());
+		}
+	}
+	
+	private void endRound() {
+		if (board.getPlayer() != null)
+			scores.get("player").add(board.getPlayer().getEnergy());
+		
+		for (MasterSquirrelBot bot : board.getMasterSquirrelBots()) {
+			String name = bot.getClass().getName();
+			if (name.startsWith("de.hsa.games.fatsquirrel.botimpls.")) {
+				name = name.substring("de.hsa.games.fatsquirrel.botimpls.".length());
+			}
+			scores.get(name).add(bot.getEnergy());
 		}
 		
-		List<Map.Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(scores.entrySet());
-		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+		List<Map.Entry<String, List<Integer>>> list = new LinkedList<Entry<String, List<Integer>>>(scores.entrySet());
+		Collections.sort(list, new Comparator<Map.Entry<String, List<Integer>>>() {
 
 			@Override
-			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
-				if (o1.getValue() > o2.getValue())
-					return 1;
-				else if (o1.getValue() < o2.getValue())
-					return -1;
-				return 0;
+			public int compare(Entry<String, List<Integer>> o1, Entry<String, List<Integer>> o2) {
+				int highest1 = 0;
+				ListIterator<Integer> i = o1.getValue().listIterator();
+				while (i.hasNext()) {
+					Integer val = i.next();
+					if (val > highest1)
+						highest1 = val;
+				}
+				
+				int highest2 = 0;
+				ListIterator<Integer> i2 = o2.getValue().listIterator();
+				while (i2.hasNext()) {
+					Integer val = i2.next();
+					if (val > highest2)
+						highest2 = val;
+				}
+				
+				return Integer.compare(highest1, highest2);
 			}
 		});
 		
 		System.out.println();
 		final Logger logger = Logger.getLogger(State.class.getName());
 		logger.info("Scores:");
-		for (Map.Entry<String, Integer> entry : list) {
-			System.out.println(entry.getKey() + "\t|\t" + entry.getValue());
-			logger.info(entry.getKey() + "\t|\t" + entry.getValue());
+		for (Map.Entry<String, List<Integer>> entry : list) {
+			StringBuilder s = new StringBuilder(entry.getKey());
+			ListIterator<Integer> i = entry.getValue().listIterator();
+			while (i.hasNext()) {
+				s.append("\t|\t" + i.next());
+			}
+			System.out.println(s.toString());
+			logger.info(s.toString());
 		}
 		
 		steps = board.getConfig().getSteps();
