3b827adfe6c99dba0a89079a66940affdcd4b703
gzazartur <richau33@gmail.com>
2018-06-10 20:27:48 +0200

    IntegrationTest

--- Strategiespiel/src/de/hsa/games/fatsquirrel/test/FlattendBoardIntegrationTest.java
+++ Strategiespiel/src/de/hsa/games/fatsquirrel/test/FlattendBoardIntegrationTest.java
@@ -11,7 +11,13 @@ import org.junit.jupiter.api.Test;
 import de.hsa.games.fatsquirrel.botapi.BotControllerFactory;
 import de.hsa.games.fatsquirrel.core.Board;
 import de.hsa.games.fatsquirrel.core.BoardConfig;
+import de.hsa.games.fatsquirrel.core.EntityType;
 import de.hsa.games.fatsquirrel.core.FlattenedBoard;
+import de.hsa.games.fatsquirrel.entities.GoodBeast;
+import de.hsa.games.fatsquirrel.entities.HandOperatedMasterSquirrel;
+import de.hsa.games.fatsquirrel.entities.HandOperatedMiniSquirrel;
+import de.hsa.games.fatsquirrel.entities.MiniSquirrel;
+import de.hsa.games.fatsquirrel.entities.Squirrel;
 import de.hsa.games.fatsquirrel.util.XY;
 
 class FlattendBoardIntegrationTest {
@@ -40,42 +46,92 @@ class FlattendBoardIntegrationTest {
 	
 	@Test
 	void testNearestSquirrel() {
-		fail("Not yet implemented");
+		HandOperatedMasterSquirrel masterSquirrel = new HandOperatedMasterSquirrel(new XY(7,7));
+		MiniSquirrel miniSquirrel = new HandOperatedMiniSquirrel(200, new XY(5, 5), masterSquirrel);
+		
+		board.insert(masterSquirrel);
+		board.insert(miniSquirrel);
+		FlattenedBoard flatBoard = new FlattenedBoard(board);
+
+		Squirrel nearestSquirrel = flatBoard.nearestSquirrel(new XY(3, 3));
+		
+		assertEquals(miniSquirrel, nearestSquirrel);
 	}
 
 	@Test
 	void testTryInsert() {
-		fail("Not yet implemented");
+		HandOperatedMasterSquirrel masterSquirrel = new HandOperatedMasterSquirrel(new XY(7,7));
+		GoodBeast goodBeast = new GoodBeast(new XY(7,7));
+		FlattenedBoard flatBoard = new FlattenedBoard(board);
+		
+		assertTrue(flatBoard.tryInsert(masterSquirrel));
+		assertFalse(flatBoard.tryInsert(goodBeast));
 	}
 
 	@Test
 	void testImplode() {
-		fail("Not yet implemented");
+		HandOperatedMasterSquirrel master = new HandOperatedMasterSquirrel(new XY(3,3));
+		HandOperatedMiniSquirrel mini = new HandOperatedMiniSquirrel(100, new XY(2,2), master);
+		
+		FlattenedBoard flatBoard = new FlattenedBoard(board);
+		
+		assertEquals(master.getEnergy(), 1000);
+		
+		flatBoard.implode(mini, 2);
+		
+		assertTrue(master.getEnergy() == 1000);
 	}
 
 	@Test
 	void testKill() {
-		fail("Not yet implemented");
+		GoodBeast goodBeast = new GoodBeast(new XY(7,7));
+		
+//		board.insert(goodBeast);
+		
+		FlattenedBoard flatBoard = new FlattenedBoard(board);
+		
+		flatBoard.kill(goodBeast);
 	}
 
 	@Test
 	void testKillAndReplace() {
-		fail("Not yet implemented");
+		GoodBeast goodBeast = new GoodBeast(new XY(7,7));
+		
+//		board.insert(goodBeast);
+		
+		FlattenedBoard flatBoard = new FlattenedBoard(board);
+		
+		flatBoard.killAndReplace(goodBeast);
 	}
 
 	@Test
 	void testGetEntityType() {
-		fail("Not yet implemented");
+		GoodBeast goodBeast = new GoodBeast(new XY(7,7));
+		
+//		board.insert(goodBeast);
+		
+		FlattenedBoard flatBoard = new FlattenedBoard(board);
+		
+		assertEquals(EntityType.GOOD_BEAST, flatBoard.getEntityType(goodBeast.getXY()));
+		assertEquals(EntityType.NONE, flatBoard.getEntityType(new XY(2,2)));
 	}
 
 	@Test
 	void testGetEntity() {
-		fail("Not yet implemented");
+		GoodBeast goodBeast = new GoodBeast(new XY(7,7));
+		
+		board.insert(goodBeast);
+		
+		FlattenedBoard flatBoard = new FlattenedBoard(board);
+		
+		assertEquals(goodBeast, flatBoard.getEntity(goodBeast.getXY()));
 	}
 
 	@Test
 	void testGetSize() {
-		fail("Not yet implemented");
+		FlattenedBoard flatBoard = new FlattenedBoard(board);
+		
+		assertEquals(new XY(20,20),flatBoard.getSize());
 	}
 
 }
